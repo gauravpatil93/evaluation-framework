e304776bd497b3c0009cc13e90f0b0e255126003
Gaurav Patil <gauravpatil93@outlook.com>
2017-03-12 19:59:44 -0400

    Implemented a working BM25 benchmark

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "=========================================="
+echo "Generating results file ................."
+echo "=========================================="
+
+python ef_ranking_document_generate.py spritzer.cbor.outlines spritzer.cbor.paragraphs output.run
+
+echo "=========================================="
+echo "Running evaluation framework............."
+echo "=========================================="
+
+python ef_evaluate.py spritzer.cbor.hierarchical.qrels output.run
