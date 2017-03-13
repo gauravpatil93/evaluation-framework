57683d2d44c45e67cb8288be69c9a8253352c502
Shilpa Dhagat <shilpa.dhagat15@gmail.com>
2017-03-12 23:28:20 -0400

    Updated to use enhanced queries

--- run.sh
+++ run.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-echo "==========================================="
-echo "Generating results file ................."
-echo "==========================================="
+echo "============================================================="
+echo "Generating results file with regular queries................."
+echo "============================================================="
 
-python ef_ranking_document_generate.py spritzer.cbor.outlines spritzer.cbor.paragraphs output.run
+python ef_ranking_document_generate.py spritzer.cbor.outlines spritzer.cbor.paragraphs output.run notenhanced
 
-echo "==========================================="
+echo "============================================================="
 echo "Running evaluation framework............."
-echo "==========================================="
+echo "============================================================="
 
 python ef_evaluate.py spritzer.cbor.hierarchical.qrels output.run
+
+echo "============================================================="
+echo "Generating results file with tagme enchanced queries........."
+echo "============================================================="
+
+python ef_ranking_document_generate.py spritzer.cbor.outlines spritzer.cbor.paragraphs output_tagme.run enhanced
+
+echo "============================================================="
+echo "Running evaluation framework on results on tagme enhanced results"
+echo "============================================================="
+
+python ef_evaluate.py spritzer.cbor.hierarchical.qrels output_tagme.run
