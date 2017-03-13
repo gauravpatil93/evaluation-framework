cb432d65e37e927e4b910b0ca6f696d22ec7e815
Shilpa Dhagat <shilpa.dhagat15@gmail.com>
2017-03-12 23:25:31 -0400

    Implemented Tagme enchanced queries
    Update the run.sh file to run regular and enhanced queries

--- ef_ranking_document_generate.py
+++ ef_ranking_document_generate.py
@@ -1,20 +1,30 @@
 import argparse
 from copy import deepcopy
 
+import tagme
 from trec_car.format_runs import *
 from trec_car.read_data import *
 
 from ef_BM25_ranking import *
 
+GCUBE_TOKEN = "bfbfb535-3683-47c0-bd11-df06d5d96726-843339462"
+
 parser = argparse.ArgumentParser()
 parser.add_argument("outline_file", type=str, help="Qualified location of the outline file")
 parser.add_argument("paragraph_file", type=str, help="Qualified location of the paragraph file")
 parser.add_argument("output_file", type=str, help="Name of the output file")
+parser.add_argument("use_enchanced_query", type=str, help="Use tageme enchanced queries")
 args = vars(parser.parse_args())
 
 query_cbor = args['outline_file']
 paragraphs_cbor = args['paragraph_file']
 output_file_name = args['output_file']
+use_enchanced_query = args['use_enchanced_query']
+
+if use_enchanced_query == "enhanced" or use_enchanced_query == "notenhanced":
+    pass
+else:
+    exit()
 
 # Pages
 pages = []
@@ -35,15 +45,31 @@ with open(paragraphs_cbor, 'rb') as f:
         paragraphs.append(tup)
     print("Gathered Paragraphs")
 
-# Generate queries in plain text
 queries = []
-for page in pages:
-    for section_path in page.flat_headings_list():
-        query_id_plain = " ".join([page.page_name] + [section.heading for section in section_path])
-        query_id_formatted = "/".join([page.page_id] + [section.headingId for section in section_path])
-        tup = (query_id_plain.lower(), query_id_formatted, query_id_plain.split())
-        queries.append(tup)
-print("Gathered Queries")
+if use_enchanced_query == "enhanced":
+    print("Generating TagMe enchanced queries")
+    # Generate tageme queries
+    for page in pages:
+        for section_path in page.flat_headings_list():
+            query_id_plain = " ".join([page.page_name] + [section.heading for section in section_path])
+            query_id_formatted = "/".join([page.page_id] + [section.headingId for section in section_path])
+            enchance_query_list = query_id_plain.split()
+            annotations = tagme.annotate(query_id_plain, GCUBE_TOKEN)
+            for annotation in annotations.get_annotations(0.1):
+                enchance_query_list += annotation.entity_title.replace('(', '').replace(')', '').split()
+            tup = (query_id_plain.lower(), query_id_formatted, list(set(enchance_query_list)))
+            print(tup)
+            queries.append(tup)
+    print("Gathered Tagme Enhanced Queries")
+if use_enchanced_query == "notenhanced":
+    # Generate queries in plain text
+    for page in pages:
+        for section_path in page.flat_headings_list():
+            query_id_plain = " ".join([page.page_name] + [section.heading for section in section_path])
+            query_id_formatted = "/".join([page.page_id] + [section.headingId for section in section_path])
+            tup = (query_id_plain.lower(), query_id_formatted, query_id_plain.split())
+            queries.append(tup)
+    print("Gathered Regular Queries")
 
 bm25_instance = BM25(queries, paragraphs)
 
